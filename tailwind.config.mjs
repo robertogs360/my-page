d5c23d2ea6ef369dc8ba394f1922e5447424d849
Roberto <robertogs360@gmail.com>
2024-02-28 11:18:09 +0100

    Add sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,12 @@ import animations from '@midudev/tailwind-animations'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        primary: 'C9C',
+        secondary: '#424'
+      }
+    },
 	},
 	plugins: [animations],
 }
